3aa90766475138cb01c966f101b56ac4fa53f62d
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-04-12 23:12:37 +0300

    Final

---
+++ Utils/index.js
@@ -0,0 +1,9 @@
+const ctrlWrapper = require("./CtrlWrapper")
+const validateBody = require("./validateBody")
+const handleMongooseError = require("./handleMongooseError")
+
+module.exports = {
+    ctrlWrapper,
+    validateBody,
+    handleMongooseError,
+}
